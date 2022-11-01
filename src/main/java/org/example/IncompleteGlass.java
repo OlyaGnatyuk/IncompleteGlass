ebab3688d91b25e3fb29d96d5564e470aae9fb01
OlyaGnatyuk <43353123+OlyaGnatyuk@users.noreply.github.com>
2022-11-01 14:52:35 +0300

    Initial commit

---
+++ src/main/java/org/example/IncompleteGlass.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class IncompleteGlass {
+    public static void main(String[] args) {
+        int fullGlassVolumeInMilliliters = 200;
+        double fullnessGlass = 0.4;
+        double waterInGlassVolume = fullGlassVolumeInMilliliters * fullnessGlass;
+    }
+}
